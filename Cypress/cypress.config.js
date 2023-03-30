139e77b7c0135ec27bd56e890c3bf6cacccd4eb9
8Kit <kit@made.llc>
2023-03-30 08:57:56 -0400

    cy.rc-tonnes

--- Cypress/cypress.config.js
+++ Cypress/cypress.config.js
@@ -2,7 +2,8 @@ module.exports = {
   'projectId': '4b7344',
   e2e: {
     baseUrl: 'https://www.demoblaze.com/',
-    account: '1234'
+    account: '1234',
+    defaultCommandTimeout: 7000,
   },
   env: {},
 }
