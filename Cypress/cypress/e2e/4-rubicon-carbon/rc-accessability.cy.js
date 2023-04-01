6689bb5a40a9424d0680a69d345a51e7f23820d5
8Kit <kit@made.llc>
2023-04-01 01:04:00 -0400

    ada

--- Cypress/cypress/e2e/4-rubicon-carbon/rc-accessability.cy.js
+++ Cypress/cypress/e2e/4-rubicon-carbon/rc-accessability.cy.js
@@ -2,33 +2,33 @@
 
 // Verify Rubicon Carbon Products WCAG Accessibility Compliance
 describe('Rubicon Carbon Products ADA Compliance', () => {
-    beforeEach(() => {
-        cy.visit('https://made.llc').as('url')
-        //cy.contains('Only necessary cookies').should('be.visible').click()
-        //cy.setCookie('cookieConsent', 'analyticsCookies=false%3B')
-        cy.injectAxe()
-        cy.wait(3000)
-    })
+  beforeEach(() => {
+    cy.visit('https://made.llc').as('url')
+    //cy.contains('Only necessary cookies').should('be.visible').click()
+    //cy.setCookie('cookieConsent', 'analyticsCookies=false%3B')
+    cy.injectAxe()
+    cy.wait(3000)
+  })
 
-    it('should test the webpage accessability is WCAG conformant', () => {
-        const axe = require('axe-core')
+  it('should test the webpage accessability is WCAG conformant', () => {
+    const axe = require('axe-core')
 
-        // use the Axe library to check the URL for WCAG conformance.
-        axe.run(url, {
-            runOnly: {
-                type: 'tag',
-                values: ['wcag2a', 'wcag2aa']
-            }
-        }, (err, results) => {
-            if (err) {
-                console.error(err)
-            } else {
-                console.log(results)
-            }
-        })
+    // use the Axe library to check the URL for WCAG conformance.
+    axe.run(url, {
+      runOnly: {
+        type: 'tag',
+        values: ['wcag2a', 'wcag2aa']
+      }
+    }, (err, results) => {
+      if (err) {
+        console.error(err)
+      } else {
+        console.log(results)
+      }
     })
+  })
 
-    it('should test the webpage accessability using Google Lighthouse audit', () => {
-        cy.lighthouse()
-    })
+  it('should test the webpage accessability using Google Lighthouse audit', () => {
+    cy.lighthouse()
+  })
 })
